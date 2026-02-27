b61250f23c95296e8410bf296bea790232be3f17
Wouter Mellema <w.mellema@iwink.nl>
2026-02-27 15:38:46 +0100

    Fixed docs and no pytest on 3.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,42 +8,44 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 
 
-project = 'Python-Betterstack-Uptime'
-copyright = '2023, iWink'
-author = 'iWink'
-release = '0.1.0'
+project = "Python-Betterstack-Uptime"
+copyright = "2023, iWink"
+author = "iWink"
+release = "2.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme', 'sphinx_mdinclude']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx_rtd_theme", "sphinx_mdinclude"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
+
+# Suppress warnings about duplicate cross-references from re-exported classes
+suppress_warnings = ["ref.python"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_css_files = ['css/custom.css']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
-autoclass_content = 'both'
+autoclass_content = "both"
 
 html_theme_options = {
-    'sticky_navigation': True,
-    'collapse_navigation': False,
-    'navigation_depth': 4,
-
+    "sticky_navigation": True,
+    "collapse_navigation": False,
+    "navigation_depth": 4,
 }
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
 }
